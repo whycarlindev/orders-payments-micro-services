f3d6704001f81cc0b4ee601b9b7634e78a3a0f39
Carlos Almeida <almeida1.carlos@gruposervices.com.br>
2026-01-27 12:00:27 -0300

    🔧env schema

---
+++ orders/src/infra/env/index.ts
@@ -0,0 +1,19 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['local', 'hml', 'prod']).default('local'),
+  PORT: z.coerce.number().default(8070),
+
+  DATABASE_CONNECTION_STRING: z.string(),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.error('Invalid environment variables!', _env.error.format())
+
+  throw new Error('Invalid environment variables')
+}
+
+export const env = _env.data
