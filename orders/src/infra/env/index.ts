c0a58b98dd8588020b98e9fb5d995e8f89cd1bec
Carlos Almeida <almeida1.carlos@gruposervices.com.br>
2026-01-27 14:25:21 -0300

    🔧update env: rabbitmq url

--- orders/src/infra/env/index.ts
+++ orders/src/infra/env/index.ts
@@ -6,6 +6,7 @@ const envSchema = z.object({
   PORT: z.coerce.number().default(8070),
 
   DATABASE_CONNECTION_STRING: z.string(),
+  RABBITMQ_URL: z.string().default('amqp://localhost'),
 })
 
 const _env = envSchema.safeParse(process.env)
